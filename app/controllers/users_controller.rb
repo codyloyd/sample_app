cfc254cc98eac91ad0da01d8aa949886bc602fa5
Cody Loyd <codyloyd@gmail.com>
2016-06-28 07:21:19 -0500

    help?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #something happens
+    # Handle a successful save.
     else
       render 'new'
     end
   end
 
-  private
-    def user_params
-      params.require(:user).permit(:username,:email,:password,:password_confirmation)
-    end
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
