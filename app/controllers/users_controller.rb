64ae02d2f87f2f48ca9686c2f5e62ad2e894d6b0
Cody Loyd <codyloyd@gmail.com>
2016-06-30 15:57:43 -0500

    finish loginoutadminetc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:edit, :update]
+before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
 before_action :correct_user, only: [:edit, :update]
+before_action :admin_user, only: [:destroy]
 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   def new
     @user = User.new
   end
@@ -30,6 +34,11 @@ before_action :correct_user, only: [:edit, :update]
       render 'edit'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
 
 private
@@ -48,5 +57,8 @@ private
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
